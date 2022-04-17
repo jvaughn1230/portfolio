cea890e6d889bf4393168f6d65e2de511c56b921
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-04-17 14:09:51 -0700

    Initial commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,7 @@
+const Projects = () => {
+    return(
+        <h2>Projects Sections</h2>
+    );
+}
+
+export default Projects;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,7 @@
+const Skills = () => {
+    return(
+        <h2>Skills Section</h2>
+    );
+}
+
+export default Skills;
